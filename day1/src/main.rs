dfa03cf08fbc106b51c39d749d5bae074772f87a
Brad Harris <brad.harris22@gmail.com>
2022-12-01 12:33:31 -0500

    fix: simplify day1 solution slightly

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,12 +3,14 @@ use utils::read_lines;
 #[derive(Debug)]
 struct Elf {
     calories: Vec<u32>,
+    total_calories: u32,
 }
 
 impl Elf {
     fn new() -> Elf {
         Elf {
             calories: Vec::new(),
+            total_calories: 0,
         }
     }
 }
@@ -21,18 +23,19 @@ fn main() {
             if let Some(n) = n {
                 let elf = acc.last_mut().unwrap();
                 elf.calories.push(n);
+                elf.total_calories = elf.total_calories + n;
             } else {
                 acc.push(Elf::new());
             }
             return acc;
         });
 
-    elves.sort_by_cached_key(|e| e.calories.iter().sum::<u32>());
+    elves.sort_by_key(|e| e.total_calories);
     elves.reverse();
 
-    let most_calories = elves.first().unwrap().calories.iter().sum::<u32>();
+    let most_calories = elves.first().unwrap().total_calories;
     println!("1 - most calories: {most_calories}");
 
-    let top3_calories_summed = elves[0..3].iter().map(|e| e.calories.iter().sum::<u32>()).sum::<u32>();
+    let top3_calories_summed = elves[0..3].iter().map(|e| e.total_calories).sum::<u32>();
     println!("2 - top 3 calories: {top3_calories_summed}");
 }
