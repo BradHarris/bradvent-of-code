d518efe510ebe83a151fa4ea4680f3bae1452d34
Brad Harris <brad.harris22@gmail.com>
2022-12-02 15:33:13 -0500

    chore: impl alternate simpler solution for day2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,4 +1,4 @@
-use std::{str::FromStr};
+use std::str::FromStr;
 
 use utils::read_lines;
 
@@ -18,7 +18,7 @@ impl FromStr for RPS {
             "B" | "Y" => Ok(RPS::Paper),
             "C" | "Z" => Ok(RPS::Scissors),
             _ => Err(format!("{s} is not a valid RPS")),
-        }   
+        }
     }
 }
 
@@ -32,9 +32,13 @@ impl RPS {
     }
 
     fn fight(&self, enemy: RPS) -> usize {
-        if enemy == self.win_to() { 6 }
-        else if enemy == self.lose_to() { 0 }
-        else { 3 }
+        if enemy == self.win_to() {
+            6
+        } else if enemy == self.lose_to() {
+            0
+        } else {
+            3
+        }
     }
 
     fn lose_to(&self) -> RPS {
@@ -72,7 +76,7 @@ impl FromStr for Round {
             "X" => Ok(Round::Lose),
             "Y" => Ok(Round::Draw),
             "Z" => Ok(Round::Win),
-            _ => Err(format!("{s} is not a valid round"))
+            _ => Err(format!("{s} is not a valid round")),
         }
     }
 }
@@ -85,9 +89,7 @@ fn main() {
             let (enemy, you) = l.split_once(" ").unwrap();
             (enemy.parse::<RPS>().unwrap(), you.parse::<RPS>().unwrap())
         })
-        .fold(0, |acc, (enemy, you)| {
-            acc + you.score() + you.fight(enemy)
-        });
+        .fold(0, |acc, (enemy, you)| acc + you.score() + you.fight(enemy));
 
     println!("1 - score: {score}");
 
@@ -96,7 +98,10 @@ fn main() {
         .map(|l| {
             let l = l.unwrap();
             let (enemy, expected) = l.split_once(" ").unwrap();
-            (enemy.parse::<RPS>().unwrap(), expected.parse::<Round>().unwrap())
+            (
+                enemy.parse::<RPS>().unwrap(),
+                expected.parse::<Round>().unwrap(),
+            )
         })
         .fold(0, |acc, (enemy, expected)| {
             acc + match expected {
@@ -107,4 +112,46 @@ fn main() {
         });
 
     println!("2 - score: {score}");
+
+    alternate_solution();
+}
+
+fn alternate_solution() {
+    let score = read_lines("./day2/input.txt")
+        .unwrap()
+        .map(|l| l.unwrap())
+        .fold(0, |acc, l| {
+            acc + match l.as_str() {
+                "A X" => 3 + 1, // rock - rock
+                "A Y" => 6 + 2, // rock - paper
+                "A Z" => 0 + 3, // rock - scissors
+                "B X" => 0 + 1, // paper - rock
+                "B Y" => 3 + 2, // paper - paper
+                "B Z" => 6 + 3, // paper - scissors
+                "C X" => 6 + 1, // scissors - rock
+                "C Y" => 0 + 2, // scissors - paper
+                "C Z" => 3 + 3, // scissors - scissors
+                _ => panic!("{l} does not match anything!"),
+            }
+        });
+    println!("alt 1 - score: {score}");
+
+    let score = read_lines("./day2/input.txt")
+        .unwrap()
+        .map(|l| l.unwrap())
+        .fold(0, |acc, l| {
+            acc + match l.as_str() {
+                "A X" => 0 + 3, // rock - lose - scissors
+                "A Y" => 3 + 1, // rock - draw - rock
+                "A Z" => 6 + 2, // rock - win - paper
+                "B X" => 0 + 1, // paper - lose - rock
+                "B Y" => 3 + 2, // paper - draw - paper
+                "B Z" => 6 + 3, // paper - win - scissors
+                "C X" => 0 + 2, // scissors - lose - paper
+                "C Y" => 3 + 3, // scissors - draw - scissors
+                "C Z" => 6 + 1, // scissors - win - rock
+                _ => panic!("{l} does not match anything!"),
+            }
+        });
+    println!("alt 2 - score: {score}");
 }
