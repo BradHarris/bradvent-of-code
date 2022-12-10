3ed547191cdb074b011fbb7e29558547b9706cc8
Brad Harris <brad.harris22@gmail.com>
2022-12-09 23:37:04 -0500

    refactor: moves core runner code around and adds benchmarks

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,45 @@
+use std::time::Instant;
+
 use crate::{solver::Solver, utils::read_input};
 
 mod solver;
 mod utils;
-use solver::*;
 
 fn main() {
+    let mut solvers: Vec<Box<dyn Solver>> = solver::get_solvers();
+
     let args: Vec<String> = std::env::args().collect();
-    let day_to_solve = args.get(1).unwrap();
+    let days_to_solve = if let Some(a) = args.get(1) {
+        let a = a.parse::<usize>().unwrap();
+        a - 1..a
+    } else {
+        0..solvers.len()
+    };
 
-    let mut solvers: Vec<Box<dyn Solver>> = vec![
-        Box::new(day1::Day1::new()),
-        Box::new(day2::Day2::new()),
-        Box::new(day3::Day3::new()),
-        Box::new(day4::Day4::new()),
-        Box::new(day5::Day5::new()),
-        Box::new(day6::Day6::new()),
-        Box::new(day7::Day7::new()),
-        Box::new(day8::Day8::new()),
-        Box::new(day9::Day9::default()),
-        // Box::new(day10::Day10::new()),
-        // Box::new(day11::Day11::new()),
-        // Box::new(day12::Day12::new()),
-        // Box::new(day13::Day13::new()),
-        // Box::new(day14::Day14::new()),
-        // Box::new(day15::Day15::new()),
-        // Box::new(day16::Day16::new()),
-        // Box::new(day17::Day17::new()),
-        // Box::new(day18::Day18::new()),
-        // Box::new(day19::Day19::new()),
-        // Box::new(day20::Day20::new()),
-        // Box::new(day21::Day21::new()),
-        // Box::new(day22::Day22::new()),
-        // Box::new(day23::Day23::new()),
-        // Box::new(day24::Day24::new()),
-        // Box::new(day25::Day25::new()),
-    ];
+    for d in days_to_solve {
+        println!("\n--- DAY {} ---", d + 1);
+        let input = read_input(&format!("./src/input/day{}.txt", d + 1));
 
-    let input = read_input(&format!("./src/input/day{day_to_solve}.txt"));
+        let solver = solvers.get_mut(d).unwrap();
 
-    let solver = solvers
-        .get_mut(day_to_solve.parse::<usize>().unwrap() - 1)
-        .unwrap();
+        let parser_start = Instant::now();
+        solver.parse(&input);
+        let parser_dur = parser_start.elapsed();
 
-    solver.parse(&input);
-    println!("day {day_to_solve} part 1: {}", solver.solve_part1());
-    println!("day {day_to_solve} part 2: {}", solver.solve_part2());
+        let part1_start = Instant::now();
+        println!("part 1: {}", solver.solve_part1());
+        let part1_dur = part1_start.elapsed();
+
+        let part2_start = Instant::now();
+        println!("part 2: {}", solver.solve_part2());
+        let part2_dur = part2_start.elapsed();
+
+        println!(
+            "parse: {:?}, part1: {:?}, part2: {:?} total: {:?}\n",
+            parser_dur,
+            part1_dur,
+            part2_dur,
+            parser_start.elapsed()
+        );
+    }
 }
