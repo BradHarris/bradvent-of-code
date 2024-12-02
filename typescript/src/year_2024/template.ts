8099f3c1baa2ea3eab1e93fa2acd63e0d5f0d0e6
Brad Harris <brad.harris22@gmail.com>
2024-12-02 11:08:48 -0500

    refactor: move typescript files into src dir

---
+++ typescript/src/year_2024/template.ts
@@ -0,0 +1,37 @@
+import { assertEquals } from "@std/assert/equals";
+
+export function parseInput(input: string) {
+    return input.split("\n");
+}
+
+export function part1(input: string) {
+    const parsed = parseInput(input);
+
+    return parsed.toString();
+}
+
+Deno.test("part 01 example", () => {
+    assertEquals(part1(EXAMPLE_INPUT), "");
+});
+
+Deno.test("part 01 input", () => {
+    assertEquals(part1(INPUT), "");
+});
+
+export function part2(input: string) {
+    const parsed = parseInput(input);
+
+    return parsed.toString();
+}
+
+Deno.test("part 02 example", () => {
+    assertEquals(part2(EXAMPLE_INPUT), "");
+});
+
+Deno.test("part 02 input", () => {
+    assertEquals(part2(INPUT), "");
+});
+
+export const EXAMPLE_INPUT = ``;
+
+export const INPUT = ``;
